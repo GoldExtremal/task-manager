cc5ac0ba4cc4a18efe0ce42ab4a0779bf9887497
Artem Mandrin <a.v.mandrin@gmai.com>
2026-03-31 16:43:42 +0300

    task manager v2

--- index.js
+++ index.js
@@ -1,29 +1,87 @@
-let task = '';
+const tasks = [];
+const completedTasks = [];
+
+const task = {
+    title: "Купить продукты",
+    description: "Молоко, хлеб, яйца",
+    isCompleted: false,
+    createdDate: new Date(),
+    completedDate: null
+};
+
+const task1 = {
+    title: "Сходить на тренировку",
+    description: "Молоко, хлеб, яйца",
+    isCompleted: true,
+    createdDate: new Date(),
+    completedDate: null
+};
+
 let completedTaskCount = 0;
 
-function showTask(task) {
-    (task === "") 
+function showTask(tasks) {
+    (tasks.length === 0)
         ? console.log("Задача отсутствует")
-        : console.log(task);
+        : tasks.forEach(task => console.log(JSON.stringify(task)));;
 }
 
 function setTask(taskDescription) {
-    (task === "") 
-        ? task = taskDescription
-        : console.log("Не могу добавит задачу, завершите или удалите предыдущую")
+    const task = { ...taskDescription };
+    tasks.push(task);
+
+    // (task === "") ? task = taskDescription : console.log("Не могу добавит задачу, завершите или удалите предыдущую")
 }
 
-function completeTask() {
-    if (task === "") {
-        console.log("Задача отсутствует");
-        return
-    }
-    task = "";
+function completeTask(index) {
+    const completedTask = tasks[index];
+    completedTask.isCompleted = true;
+    completedTask.completedDate = Date.now();
+    completedTasks.push(completedTask);
     completedTaskCount++;
+
+    // if (task === "") {
+    //     console.log("Задача отсутствует");
+    //     return
+    // }
+    // task = "";
 }
 
-function deleteTask() {
-    (task === "") 
-        ? console.log("Задача отсутствует") 
-        : task = "";
+function deleteTask(index) {
+    if (typeof index !== "number") return
+    const task = tasks[index];
+    if (!task.isCompleted) {
+        const isApprove = prompt('Таска ещё не выполнена, удалить?', 'да/нет');
+        if (isApprove === 'да') {
+            tasks.splice(index, 1);
+        }else if (isApprove === 'нет') {
+            return
+        }
+    } else {
+        tasks.splice(index, 1);
+    }
+
+    // (task === "")
+    //     ? console.log("Задача отсутствует")
+    //     : task = "";
 }
+
+function clearTasks() {
+    tasks.splice(0)
+}
+
+setTask(task);
+setTask(task1);
+// showTask(tasks);
+console.log(tasks)
+setTimeout( () => {
+    deleteTask(1)
+    console.log(tasks);
+}, 5000);
+setTimeout( () => {
+    setTask(task1);
+    console.log(tasks);
+}, 10000);
+setTimeout( () => {
+    clearTasks();
+    console.log(tasks);
+}, 15000);
